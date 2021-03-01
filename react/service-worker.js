644f3a976d86b717a90de9dde75435b5abcd6877
felipefialho <hi@felipefialho.com>
2021-03-01 18:53:58 +0000

    Deploying to gh-pages from  @ fcfebd7df2d158843395e798f0a629ad028f8d93 🚀

--- react/service-worker.js
+++ react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.48d7d2ddb3d39061147840ba1dc965dd.js"
+  "precache-manifest.a2c557d0f54a32102bf6d28da365b4cc.js"
 );
 
 self.addEventListener('message', (event) => {
